389597ac61d319409594a3d34785bc9afd96e9f8
Ilya Fateev <fat32nov@gmail.com>
2014-09-16 18:47:13 +0400

    Prototyping schedule

--- prototype/transport_nov_angularjs/app/js/app.js
+++ prototype/transport_nov_angularjs/app/js/app.js
@@ -1,3 +1,21 @@
 'use strict';
 
 /* App Module */
+var transportApp = angular.module('transportApp', [
+    'ngRoute',
+    'transportControllers'
+]);
+
+transportApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'html/main_view.html',
+            controller: 'MainViewController'
+        }).
+        when('/buses/:busId', {
+            templateUrl: 'html/bus_view.html',
+            controller: 'BusScheduleController'
+        }).otherwise({
+            redirectTo: '/'
+        });
+    }]);
--- prototype/transport_nov_angularjs/app/js/controllers.js
+++ prototype/transport_nov_angularjs/app/js/controllers.js
@@ -2,17 +2,65 @@
 
 /* Controllers */
 
-var transportApp = angular.module('transportApp', []);
+var transportControllers = angular.module('transportControllers', []);
 
-transportApp.controller('BusController', function($http) {
-    var busController = this;
-    $http({method: 'GET', url: 'transport/buses.json'}).
-        success(function(data) {
-            busController.buses = data;
+transportControllers.controller('MainViewController', ['$scope', '$http', 
+    function($scope, $http) {
+        $http.get('transport/buses.json').
+            success(function(data) {
+                $scope.buses = data;
         });    
     
-    $http({method: 'GET', url: 'transport/stations.json'}).
-        success(function(data) {
-            busController.stations = data;
+        $http.get('transport/stations.json').
+            success(function(data) {
+                $scope.stations = data;
         });    
-});
+}]);
+
+transportControllers.controller('BusScheduleController', ['$scope', '$http', '$routeParams', '$filter',
+    function($scope, $http, $routeParams, $filter) {
+        $scope.busId = $routeParams.busId;
+        $http.get('transport/schedules/' + $scope.busId + ".json").
+            success(function(data) {
+             // console.log(data); 
+             $scope.busSchedule = data; 
+        });
+        $http.get('transport/buses.json').
+            success(function(data) {
+                // console.log(data); 
+            $scope.buses = data; 
+            $scope.stationsSchedule = []; 
+            for (var x=0; x < 100; x++){
+                $scope.stationsSchedule[x] = [];
+            }
+            for (var i=0; i < data.length; i++) { 
+                if (data[i].id === $scope.busId) {
+                    $scope.currentBus = data[i];
+                }
+                // loop through the current bus schedule (bus instance) 
+                for (var j=0; j < data[i].schedule.length; j++) {
+                    // loop through the bus instance's times 
+                    for(var x=0; x < data[i].schedule[j].length; x++) {
+                        $scope.stationsSchedule[x][j] = data[i].schedule[j][x]; 
+                    }     
+                }
+            }
+            console.log($scope.stationsSchedule);
+        });
+        this.isStationSearched = function(station) {
+            var fitsQuery = [];
+            fitsQuery = $filter('filter')($scope.currentBus.stations, $scope.query);
+            if (fitsQuery.length) {             
+                for (var i=0; i < fitsQuery.length; i++){
+                    console.log(station);
+                    console.log(fitsQuery[i]);
+                    if (fitsQuery[i] === station) {
+                        this.currentStation = i; 
+                        return true; 
+                    }
+                }
+            }
+            return false;
+             //return $filter('filter')($scope.currentBus.stations, $scope.query).length;
+        }
+}]);
