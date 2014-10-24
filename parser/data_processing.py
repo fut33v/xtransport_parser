5f7a96c972bac6fd198f87adc2fbd2f82e0e382d
Ilya Fateev <fat32nov@gmail.com>
2014-10-24 14:22:58 +0400

    Removing stations that is extra (case of same stations for weekend workdays)

--- parser/data_processing.py
+++ parser/data_processing.py
@@ -43,20 +43,23 @@ if __name__ == "__main__":
     for filename in onlyfiles:
         filename = TRANSPORT_DIR + filename
         transport = parser_utils.load_json_file(filename)
+
         # print transport['name']
-        if transport['id'] == "bus_27a":
-            if 'stations_workdays' in transport:
-                for station in transport['stations_workdays']:
-                    print station['name'] + "|"
-            if 'stations_weekend' in transport:
-                for station in transport['stations_weekend']:
-                    print station['name'] + "|"
+        # if transport['id'] == "bus_27a":
+        #     if 'stations_workdays' in transport:
+        #         for station in transport['stations_workdays']:
+        #             print station['name'] + "|"
+        #     if 'stations_weekend' in transport:
+        #         for station in transport['stations_weekend']:
+        #             print station['name'] + "|"
 
         do_replace(transport, replace)
 
         if 'stations_weekend' in transport and 'stations_workdays' in transport:
             if transport['stations_weekend'] == transport['stations_workdays']:
                 transport['stations'] = transport['stations_weekend']
+                transport.pop('stations_workdays')
+                transport.pop('stations_weekend')
                 print "stations is the same"
         print "##############################"
         parser_utils.save_json_file(filename, transport)
