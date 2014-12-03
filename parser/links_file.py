3f127ef8cdce774ed5e4978b5a64430b04fbe78a
Ilya Fateev <fat32nov@gmail.com>
2014-12-03 15:39:18 +0400

    parser: fixing links.json file for the greatest ios developer

--- parser/links_file.py
+++ parser/links_file.py
@@ -8,9 +8,9 @@ urban_links = [
     "http://53bus.ru/json/iphone/" + t['id'] + ".json" for t in transport
 ]
 links = {
-    'urban_links': urban_links + ["http://53bus.ru/json/transport.json"],
+    'urban_links': urban_links,
     'suburban_links': ["http://53bus.ru/json/suburban_transport.json"],
-    'service': "http://53bus.ru/json/iphone/links.json"
+    'service': "http://53bus.ru/json/transport.json"
 }
 print links
 open('json/iphone/links.json', 'w').write(json_pretty_dumps(links))
