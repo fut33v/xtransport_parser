6c540cb4e270c9703aeff26f07ace8f0b324eb2c
Ilya Fateev <fat32nov@gmail.com>
2014-12-03 15:35:45 +0400

    parser: links.json for the greatest ios developer, again

--- parser/links_file.py
+++ parser/links_file.py
@@ -4,5 +4,13 @@ from parser_utils import json_pretty_dumps
 
 transport = json.loads(open('json/transport.json', 'r').read())
 transport = transport['buses'] + transport['trolleys']
-links = ["http://53bus.ru/json/iphone/" + t['id'] + ".json" for t in transport]
-open('json/iphone/links.json', 'w').write(json_pretty_dumps({'links': links}))
+urban_links = [
+    "http://53bus.ru/json/iphone/" + t['id'] + ".json" for t in transport
+]
+links = {
+    'urban_links': urban_links + ["http://53bus.ru/json/transport.json"],
+    'suburban_links': ["http://53bus.ru/json/suburban_transport.json"],
+    'service': "http://53bus.ru/json/iphone/links.json"
+}
+print links
+open('json/iphone/links.json', 'w').write(json_pretty_dumps(links))
